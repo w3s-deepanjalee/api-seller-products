6bdb1bdd7591567f42b77b1eec4791cf0cb0c17c
w3sdeepanjalee <deepanjalee@w3ssolutions.com>
2023-05-05 18:08:08 +0530

    remove comments

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -36,8 +36,6 @@
                 </div>
             @endif
 
-                {{-- {{ dd(Crypt::encrypt(3)) }} --}}
-                {{-- {{ dd(Crypt::encrypt(5)) }} --}}
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
