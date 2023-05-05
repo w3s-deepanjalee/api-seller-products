102a44f674dcc28fec1f3a2aaa75e664ae226171
w3sdeepanjalee <deepanjalee@w3ssolutions.com>
2023-05-05 13:16:43 +0530

    api generate

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\CommonApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('product/get_details/{sellerProductId}', [CommonApiController::class, 'productDetails']);
+Route::get('seller/get_products/{seller_id}', [CommonApiController::class, 'getProductsBySeller']);
+Route::get('product/get_seller_details/{sellerProductId}', [CommonApiController::class, 'getSellerByProduct']);
+
