cda1d6aa7cf2fd6eafd38a1035f0ad66e36d4964
w3sdeepanjalee <deepanjalee@w3ssolutions.com>
2023-05-05 18:06:23 +0530

    remove comments

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/request-docs', function () {
-    return redirect('/request-docs');
-})->middleware('auth.basic');
+
